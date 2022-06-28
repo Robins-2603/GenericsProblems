72c2854e8422785c9d0ebb3455d47a3a14a793e3
Robins-2603 <robinsforwork@gmail.com>
2022-06-29 02:10:17 +0530

    Added: prog for finding max integer

--- src/com/genericsProg/Maximum.java
+++ src/com/genericsProg/Maximum.java
@@ -1,9 +1,33 @@
 package com.genericsProg;
 
+import java.util.Scanner;
+
 public class Maximum {
     public static void main(String[] args) {
         System.out.println("------Welcome to Generics problem program--------");
+        // importing scanner for user input
+        Scanner sc = new Scanner(System.in);
+        Integer a = sc.nextInt();
+        Integer b = sc.nextInt();
+        Integer c = sc.nextInt();
+
+        //storing user input in Array named numb
+        Integer[] numb = new Integer[]{a,b,c};
+
+        //calling function of findMaxFromInteger
+        Maximum.findMaxFromInteger(numb);
 
     }
-    
+
+    // Creating function for finding maximum value of user input
+    public static void findMaxFromInteger(Integer []numb) {
+
+        Integer max = numb[0];
+        if (numb[1].compareTo(max)>0)
+            max = numb[1];
+        if(numb[2].compareTo(max)>0)
+            max = numb[2];
+        System.out.println("Maximum number is " +max);
+
+    }
 }
