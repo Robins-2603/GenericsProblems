35e3675ababf2eecb99e09961c2c4c70a4874cd7
Robins-2603 <robinsforwork@gmail.com>
2022-06-29 00:47:51 +0530

    Added: Welcome message

---
+++ src/com/genericsProg/Maximum.java
@@ -0,0 +1,9 @@
+package com.genericsProg;
+
+public class Maximum {
+    public static void main(String[] args) {
+        System.out.println("------Welcome to Generics problem program--------");
+
+    }
+    
+}
